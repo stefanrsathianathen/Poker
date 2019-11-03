bc3ce2647084078a3c83d0fe64240a6691058edd
Stefan R. Sathianathen <stefansathianathen@gmail.com>
2019-11-03 15:45:37 +1100

    card, deck and basic player done

---
+++ core.py
@@ -0,0 +1,57 @@
+class Card:
+    def __init__(self, suit, val):
+        self.suit = suit
+        self.value = val
+    
+    def show(self):
+        if self.value == 1:
+            trueValue = "Ace"
+        elif self.value == 11:
+            trueValue = "Jack"
+        elif self.value == 12:
+            trueValue = "Queen"
+        elif self.value == 13:
+            trueValue = "King"
+        else:
+            trueValue = str(self.value)
+        print("{} of {}".format(trueValue, self.suit))
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        self.build()
+
+    def build(self):
+        for s in ["Spades", "Clubs", "Diamonds", "Hearts"]:
+            for v in range(1, 14):
+                self.cards.append(Card(s, v))
+    
+    def show(self):
+        for c in self.cards:
+            c.show()
+
+    def shuffle(self):
+        import random
+        for i in range(len(self.cards) - 1, 0, -1):
+            r = random.randint(0, i)
+            self.cards[i], self.cards[r] = self.cards[r], self.cards[i]
+    
+    def drawCard(self):
+        return self.cards.pop()
+    
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
+    
+    def draw(self, deck):
+        self.hand.append(deck.drawCard())
+        self.hand.append(deck.drawCard())
+        return self
+    
+    def showHand(self):
+        for card in self.hand:
+            card.show()
+
+    def reset(self):
+        self.hand = []
